5424bd10a56d6ea73fac95bdec1d85453e883c28
ialexanderbrito <xanderzinho26@gmail.com>
2022-03-25 16:54:57 -0300

    feat: add app.tsx import context

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,24 @@
 import { BrowserRouter } from 'react-router-dom';
 
+import { AuthProvider } from 'contexts/Auth';
 import { ThemeProvider } from 'contexts/Theme';
 import { ToastProvider } from 'contexts/Toast';
 
 import { MainRoutes } from './routes';
 
+import 'react-day-picker/lib/style.css';
 import 'styles/global.scss';
 
 export function App() {
   return (
-    <ToastProvider>
-      <ThemeProvider>
-        <BrowserRouter>
-          <MainRoutes />
-        </BrowserRouter>
-      </ThemeProvider>
-    </ToastProvider>
+    <BrowserRouter>
+      <AuthProvider>
+        <ToastProvider>
+          <ThemeProvider>
+            <MainRoutes />
+          </ThemeProvider>
+        </ToastProvider>
+      </AuthProvider>
+    </BrowserRouter>
   );
 }
