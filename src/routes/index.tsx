2909c4f9c7c612b3819a6cdb9e44cec623838f61
ialexanderbrito <xanderzinho26@gmail.com>
2022-03-25 17:37:33 -0300

    feat: add new routes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,11 +1,21 @@
 import { Routes, Route } from 'react-router-dom';
 
-import { Homepage } from 'pages/Home/index';
+import { ForgetPassword } from 'pages/ForgetPassword';
+import { Home } from 'pages/Home';
+import { Login } from 'pages/Login';
+import { Profile } from 'pages/Profile';
+import { Register } from 'pages/Register';
+import { ResetPassword } from 'pages/ResetPassword';
 
 export function MainRoutes() {
   return (
     <Routes>
-      <Route path="/" element={<Homepage />} />
+      <Route path="/" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+      <Route path="/home" element={<Home />} />
+      <Route path="/profile" element={<Profile />} />
+      <Route path="/forget-password" element={<ForgetPassword />} />
+      <Route path="/reset-password" element={<ResetPassword />} />
     </Routes>
   );
 }
