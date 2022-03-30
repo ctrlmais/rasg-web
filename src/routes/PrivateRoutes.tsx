b44f9fc8ed44186e781b10a1f2aae2e30bc331a7
ialexanderbrito <xanderzinho26@gmail.com>
2022-03-30 20:36:26 -0300

    feat: add new route

--- src/routes/PrivateRoutes.tsx
+++ src/routes/PrivateRoutes.tsx
@@ -4,12 +4,14 @@ import { Home } from 'pages/Home';
 import { NotFound } from 'pages/NotFound';
 import { Profile } from 'pages/Profile';
 import { ResetPassword } from 'pages/ResetPassword';
+import { Schedule } from 'pages/Schedule';
 
 export function PrivateRoutes() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/profile" element={<Profile />} />
+      <Route path="/p/:id" element={<Schedule />} />
       <Route path="/reset-password" element={<ResetPassword />} />
       <Route path="*" element={<NotFound />} />
     </Routes>
