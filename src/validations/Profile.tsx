b7ad9a4f22ca4be10970f6d850c081bd025d93bd
ialexanderbrito <xanderzinho26@gmail.com>
2022-03-31 14:12:22 -0300

    fix: validation profile password

--- src/validations/Profile.tsx
+++ src/validations/Profile.tsx
@@ -3,15 +3,13 @@ import * as yup from 'yup';
 export const profileSchema = yup.object({
   nome: yup.string().required('O nome é obrigatório'),
   email: yup.string().email('Email inválido').required('Campo obrigatório'),
-  password: yup.string().min(6, 'Senha deve ter no mínimo 6 caracteres'),
-  newPassword: yup.string().when('password', {
-    is: (val: string | any[]) => !!val.length,
-    then: yup.string().min(6, 'Senha deve ter no mínimo 6 caracteres'),
-    otherwise: yup.string(),
-  }),
-  confirmPassword: yup.string().when('newPassword', {
-    is: (val: string | any[]) => !!val.length,
-    then: yup.string().oneOf([yup.ref('newPassword')], 'As senhas não conferem'),
-    otherwise: yup.string(),
-  }),
+  password: yup.string(),
+  newPassword: yup
+    .string()
+    .when('password', (password, field) => (password ? field.required('Campo obrigatório') : field)),
+  confirmPassword: yup
+    .string()
+    .when('newPassword', (newPassword, field) =>
+      newPassword ? field.required('Campo obrigatório').oneOf([yup.ref('newPassword')]) : field,
+    ),
 });
