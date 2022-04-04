c8ca7f71e40fcdf20bbe569c54b3faaf8bd2f91b
ialexanderbrito <xanderzinho26@gmail.com>
2022-04-04 09:40:33 -0300

    fix: new react 18

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,11 @@
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 
 import { App } from './App';
 
 import 'styles/global.scss';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const container = document.getElementById('root') as HTMLElement;
+
+const root = createRoot(container);
+
+root.render(<App />);
