af362ed2c9b7056b3271f2738941456424b25d71
ialexanderbrito <xanderzinho26@gmail.com>
2022-04-04 11:43:48 -0300

    fix: import context and css

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,26 @@
 import { BrowserRouter } from 'react-router-dom';
 
 import { AuthProvider } from 'contexts/Auth';
-import { BarbeiroProvider } from 'contexts/Barbeiro';
 import { ThemeProvider } from 'contexts/Theme';
 import { ToastProvider } from 'contexts/Toast';
+import { UserProvider } from 'contexts/User';
 
 import { MainRoutes } from './routes';
 
-import 'react-day-picker/lib/style.css';
+import 'react-day-picker/dist/style.css';
 import 'styles/global.scss';
 
 export function App() {
   return (
     <BrowserRouter>
       <AuthProvider>
-        <BarbeiroProvider>
+        <UserProvider>
           <ToastProvider>
             <ThemeProvider>
               <MainRoutes />
             </ThemeProvider>
           </ToastProvider>
-        </BarbeiroProvider>
+        </UserProvider>
       </AuthProvider>
     </BrowserRouter>
   );
