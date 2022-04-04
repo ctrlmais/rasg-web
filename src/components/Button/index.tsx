36e71ff836300a7a3225083277e790de74657bb5
ialexanderbrito <xanderzinho26@gmail.com>
2022-04-04 10:17:24 -0300

    feat: add disabled button

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -3,7 +3,7 @@ import { ButtonProps } from 'types/IComponents';
 import styles from './Button.module.scss';
 export function Button(props: ButtonProps) {
   return (
-    <button type={props.type} className={styles.button} onClick={props.onClick}>
+    <button type={props.type} className={styles.button} onClick={props.onClick} disabled={props.disabled}>
       {props.children}
     </button>
   );
