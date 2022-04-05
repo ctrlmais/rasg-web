b8873fd0bc20d2042651e5989c4fc9812ca706d2
ialexanderbrito <xanderzinho26@gmail.com>
2022-04-05 20:15:14 -0300

    feat: add hook overlay

---
+++ src/hooks/useOverlay.tsx
@@ -0,0 +1,32 @@
+import { useUser } from 'contexts/User';
+
+export function useOverlay() {
+  const { barbeiro, generateGoogleCalendarEvent, startDate, endDate, selectDayFormatted, selectHours } = useUser();
+
+  function handleGoogleCalendar() {
+    const title = `Barba, cabelo e bigode com ${barbeiro?.nome}`;
+    const description = `Barbearia do ${barbeiro?.nome}`;
+    const location = '';
+
+    generateGoogleCalendarEvent(title, startDate, endDate, description, location);
+  }
+
+  const hourFormattedCalendar = selectHours.replace(':', '');
+  const hourFormattedCalendarEnd = Number(hourFormattedCalendar) + 100;
+  const hourFormattedCalendarEndFormatted = `${hourFormattedCalendarEnd
+    .toString()
+    .substring(0, 2)}:${hourFormattedCalendarEnd.toString().substring(2, 4)}`;
+
+  const event = {
+    title: `Barba, cabelo e bigode com ${barbeiro?.nome}`,
+    description: `Barbearia do ${barbeiro?.nome}`,
+    startTime: `${selectDayFormatted}T${selectHours}`,
+    endTime: `${selectDayFormatted}T${hourFormattedCalendarEndFormatted}`,
+    location: '',
+  };
+
+  return {
+    handleGoogleCalendar,
+    event,
+  };
+}
