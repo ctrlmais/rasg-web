0d33d0943551bd33e84af8c03f0e2b69ddca8385
ialexanderbrito <xanderzinho26@gmail.com>
2022-04-05 10:08:46 -0300

    fix: styles report bug

--- src/pages/ReportBug/index.tsx
+++ src/pages/ReportBug/index.tsx
@@ -3,7 +3,6 @@ import { FiCheck, FiMessageSquare } from 'react-icons/fi';
 import { useForm } from '@formspree/react';
 
 import { Button } from 'components/Button';
-import { Header } from 'components/Header';
 import { Input } from 'components/Input';
 import { Overlay } from 'components/Overlay';
 
@@ -29,44 +28,40 @@ export function ReportBug() {
   }
 
   return (
-    <>
-      <div className={styles.home} data-theme={theme}>
-        <Header back />
+    <div className={styles.home} data-theme={theme}>
+      <div className={styles.container}>
+        <form onSubmit={handleSubmit}>
+          <h2 className={styles.title}>Enviar bug</h2>
 
-        <div className={styles.container}>
-          <form onSubmit={handleSubmit}>
-            <h2 className={styles.title}>Enviar bug</h2>
+          <div className={styles.bugContainer}>
+            <label className={styles.label}>Qual página você viu o bug?</label>
+            <Input
+              type="text"
+              name="page"
+              placeholder="Qual página você viu o bug?"
+              value={formikReportBug.values.page}
+              icon={<FiMessageSquare color="#666360" size={24} />}
+              onChange={formikReportBug.handleChange}
+              onBlur={formikReportBug.handleBlur}
+            />
 
-            <div className={styles.bugContainer}>
-              <label className={styles.label}>Qual página você viu o bug?</label>
-              <Input
-                type="text"
-                name="page"
-                placeholder="Qual página você viu o bug?"
-                value={formikReportBug.values.page}
-                icon={<FiMessageSquare color="#666360" size={24} />}
-                onChange={formikReportBug.handleChange}
-                onBlur={formikReportBug.handleBlur}
-              />
+            <label className={styles.label}>O que aconteceu?</label>
+            <textarea
+              className={styles.area}
+              name="message"
+              value={formikReportBug.values.message}
+              rows={10}
+              placeholder="O que aconteceu?"
+              onChange={formikReportBug.handleChange}
+              onBlur={formikReportBug.handleBlur}
+            />
+          </div>
 
-              <label className={styles.label}>O que aconteceu?</label>
-              <textarea
-                className={styles.area}
-                name="message"
-                value={formikReportBug.values.message}
-                rows={10}
-                placeholder="O que aconteceu?"
-                onChange={formikReportBug.handleChange}
-                onBlur={formikReportBug.handleBlur}
-              />
-            </div>
-
-            <div className={styles.buttonContainer}>
-              <Button type="submit">Enviar relatório</Button>
-            </div>
-          </form>
-        </div>
+          <div className={styles.buttonContainer}>
+            <Button type="submit">Enviar relatório</Button>
+          </div>
+        </form>
       </div>
-    </>
+    </div>
   );
 }
