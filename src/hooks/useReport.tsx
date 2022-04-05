353d0db586cfdd51b053f283aefaad8026ab7a53
ialexanderbrito <xanderzinho26@gmail.com>
2022-04-05 10:09:00 -0300

    feat: add hook use report modal

--- src/hooks/useReport.tsx
+++ src/hooks/useReport.tsx
@@ -1,3 +1,5 @@
+import { useState } from 'react';
+
 import { useFormik } from 'formik';
 
 export function useReport() {
@@ -11,7 +13,44 @@ export function useReport() {
     },
   });
 
+  const [modalIsOpen, setIsOpen] = useState(false);
+
+  function openModal() {
+    setIsOpen(true);
+  }
+
+  function closeModal() {
+    setIsOpen(false);
+  }
+
+  const customStyles = {
+    content: {
+      inset: 'initial',
+      border: 'none',
+      background: '#312e38',
+      overflow: 'auto',
+      borderRadius: '4px',
+      outline: 'none',
+      padding: '0px',
+      height: '65vh',
+      width: '25rem',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    overlay: {
+      display: 'flex',
+      inset: '0px',
+      alignItems: 'center',
+      justifyContent: 'center',
+      backgroundColor: 'rgba(0, 0, 0, 0.7)',
+    },
+  };
+
   return {
     formikReportBug,
+    modalIsOpen,
+    openModal,
+    closeModal,
+    customStyles,
   };
 }
