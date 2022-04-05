ca0d479e2377a9d47a93ce57a78613b82d1a0773
ialexanderbrito <xanderzinho26@gmail.com>
2022-04-04 22:04:30 -0300

    feat: add nome barbeiro

--- src/components/CardCliente/index.tsx
+++ src/components/CardCliente/index.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react';
-import { BsClock } from 'react-icons/bs';
+import { BsClock, BsScissors } from 'react-icons/bs';
 
 import Avvvatars from 'avvvatars-react';
 import { ClienteMetadata } from 'types/IContext';
@@ -54,8 +54,9 @@ export function CardCliente({ cliente, onClick }: Props) {
       <div className={styles.containerInfo}>
         <h2 className={styles.title}>{name || cliente?.client_name}</h2>
         <strong className={styles.info}>
-          <BsClock color="#FF9000" size={16} style={{ marginRight: '12px' }} />
-          {cliente?.hour}
+          <BsClock color="#FF9000" size={16} style={{ marginRight: '8px' }} />
+          {cliente?.hour} | <BsScissors color="#FF9000" size={16} style={{ marginLeft: '6px', marginRight: '3px' }} />
+          {cliente?.barber_name}
         </strong>
       </div>
     </div>
