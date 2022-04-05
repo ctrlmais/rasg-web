b36d1471259ccf495036672d7b81a15b28ee0ff5
ialexanderbrito <xanderzinho26@gmail.com>
2022-04-05 20:14:07 -0300

    feat: add button calendar

--- src/components/Overlay/index.tsx
+++ src/components/Overlay/index.tsx
@@ -1,29 +1,61 @@
+import { isAndroid, isWindows, isIOS, isMacOs } from 'react-device-detect';
+import ICalendarLink from 'react-icalendar-link';
+import { SiGooglecalendar, SiApple } from 'react-icons/si';
+
+import { OverlayProps } from 'types/IComponents';
+
 import { Button } from 'components/Button';
+
+import { useOverlay } from 'hooks/useOverlay';
 
 import styles from './Overlay.module.scss';
 
-type Props = {
-  title: string;
-  description: string;
-  children: React.ReactNode;
-};
+export function Overlay(props: OverlayProps) {
+  const { handleGoogleCalendar, event } = useOverlay();
 
-export function Overlay(props: Props) {
   return (
-    <div className={styles.overlay}>
-      <div className={styles.containerOverlay}>
-        {props.children}
-        <h1>{props.title}</h1>
-        <p>{props.description}</p>
-        <Button
-          type="button"
-          onClick={() => {
-            window.location.replace('/');
-          }}
-        >
-          Ok
-        </Button>
+    <>
+      <div className={styles.overlay}>
+        <div className={styles.containerOverlay}>
+          {props.children}
+          <h1>{props.title}</h1>
+          <p>{props.description}</p>
+
+          {props.calendar && (
+            <>
+              {(isAndroid || isWindows) && (
+                <>
+                  <button
+                    className={styles.google}
+                    type="button"
+                    onClick={() => {
+                      handleGoogleCalendar();
+                    }}
+                  >
+                    <SiGooglecalendar />
+                    Adicionar ao Google Calendar
+                  </button>
+                </>
+              )}
+              {(isIOS || isMacOs) && (
+                <button className={styles.apple} type="button">
+                  <SiApple />
+                  <ICalendarLink event={event}>Adicionar ao Apple Calendar</ICalendarLink>
+                </button>
+              )}
+            </>
+          )}
+
+          <Button
+            type="button"
+            onClick={() => {
+              window.location.replace('/');
+            }}
+          >
+            Ok
+          </Button>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
