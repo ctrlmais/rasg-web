b28d94223bb77805160d180a928c221cbbd58da7
ialexanderbrito <xanderzinho26@gmail.com>
2022-04-06 09:06:19 -0300

    feat: add component navbar

---
+++ src/components/Navbar/index.tsx
@@ -0,0 +1,28 @@
+import { IoArrowBackOutline } from 'react-icons/io5';
+import { useNavigate } from 'react-router-dom';
+
+import logo from 'assets/Logo.png';
+import { NavBarProps } from 'types/IComponents';
+
+import styles from './Navbar.module.scss';
+
+export function Navbar(props: NavBarProps) {
+  const navigate = useNavigate();
+  return (
+    <>
+      <nav className={styles.navbar}>
+        {props.back && (
+          <IoArrowBackOutline
+            size={24}
+            className={styles.icon}
+            onClick={() => {
+              navigate('/');
+            }}
+          />
+        )}
+        {props.logo && <img src={logo} alt="Logo" className={styles.logo} />}
+        <ul className={styles.navbarNav}>{props.children}</ul>
+      </nav>
+    </>
+  );
+}
