6cd9160ae0e93f1a2984ffa1bcde23a41d97d450
ialexanderbrito <xanderzinho26@gmail.com>
2022-04-06 20:08:40 -0300

    feat: add versão desktop barbeiro

--- src/pages/Barbeiro/index.tsx
+++ src/pages/Barbeiro/index.tsx
@@ -29,8 +29,38 @@ export function Barbeiro() {
 
   return (
     <>
-      {visible ? (
-        <>
+      <div className={styles.containerBarbeiro}>
+        {visible ? (
+          <>
+            <div className={styles.containerRightCalendar}>
+              <div className={styles.containerCalendar}>
+                <button
+                  className={styles.button}
+                  type="button"
+                  onClick={() => {
+                    setVisible(!visible);
+                  }}
+                >
+                  Ocultar calendário
+                </button>
+              </div>
+              <div className={styles.calendar}>
+                <DayPicker
+                  mode="single"
+                  locale={ptBR}
+                  fromMonth={new Date()}
+                  selected={selectDay}
+                  onDayClick={(day) => {
+                    setSelectDay(day);
+                  }}
+                  modifiers={{
+                    available: { dayOfWeek: [0, 1, 2, 3, 4, 5, 6] },
+                  }}
+                />
+              </div>
+            </div>
+          </>
+        ) : (
           <div className={styles.containerCalendar}>
             <button
               className={styles.button}
@@ -39,93 +69,69 @@ export function Barbeiro() {
                 setVisible(!visible);
               }}
             >
-              Ocultar calendário
+              Exibir calendário
             </button>
           </div>
-          <div className={styles.calendar}>
-            <DayPicker
-              mode="single"
-              locale={ptBR}
-              fromMonth={new Date()}
-              selected={selectDay}
-              onDayClick={(day) => {
-                setSelectDay(day);
-              }}
-              modifiers={{
-                available: { dayOfWeek: [0, 1, 2, 3, 4, 5, 6] },
-              }}
-            />
+        )}
+        <div className={styles.list}>
+          <div className={styles.containerHorarioAgendados}>
+            <h2 className={styles.titleHome}>Horários agendados</h2>
+            <p className={styles.infoText}>
+              Hoje | Dia {new Date().getDate()} | {getDiaSemana()}
+            </p>
           </div>
-        </>
-      ) : (
-        <div className={styles.containerCalendar}>
-          <button
-            className={styles.button}
-            type="button"
-            onClick={() => {
-              setVisible(!visible);
-            }}
-          >
-            Exibir calendário
-          </button>
+          {selectDayFormatted >= atualDayFormatted ? (
+            <>
+              <h2 className={styles.shift}>Atendimento a seguir</h2>
+              {getFirstCliente() && <CardCliente first cliente={getFirstCliente()} />}
+
+              <div className={styles.containerList}>
+                {getClientesMorning().length > 0 && (
+                  <>
+                    <h3 className={styles.shift}>
+                      Manhã
+                      <div className={styles.line} />
+                    </h3>
+                    {getClientesMorning().map((cliente: ClienteMetadata) => (
+                      <CardCliente key={cliente.id} cliente={cliente} />
+                    ))}
+                  </>
+                )}
+
+                {getClientesAfternoon().length > 0 && (
+                  <>
+                    <h2 className={styles.shift}>
+                      Tarde
+                      <div className={styles.line} />
+                    </h2>
+                    {getClientesAfternoon().map((cliente: ClienteMetadata) => (
+                      <CardCliente key={cliente.id} cliente={cliente} />
+                    ))}
+                  </>
+                )}
+
+                {getClientesNight().length > 0 && (
+                  <>
+                    <h2 className={styles.shift}>
+                      Noite
+                      <div className={styles.line} />
+                    </h2>
+                    {getClientesNight().map((cliente: ClienteMetadata) => (
+                      <CardCliente key={cliente.id} cliente={cliente} />
+                    ))}
+                  </>
+                )}
+              </div>
+            </>
+          ) : (
+            <>
+              {clientes.map((cliente: ClienteMetadata) => (
+                <CardCliente key={cliente.id} cliente={cliente} />
+              ))}
+            </>
+          )}
         </div>
-      )}
-      <div className={styles.containerHorarioAgendados}>
-        <h2 className={styles.titleHome}>Horários agendados</h2>
-        <p className={styles.infoText}>
-          Hoje | Dia {new Date().getDate()} | {getDiaSemana()}
-        </p>
       </div>
-      {selectDayFormatted >= atualDayFormatted ? (
-        <>
-          <h2 className={styles.shift}>Atendimento a seguir</h2>
-          {getFirstCliente() && <CardCliente cliente={getFirstCliente()} />}
-
-          <div className={styles.containerList}>
-            {getClientesMorning().length > 0 && (
-              <>
-                <h3 className={styles.shift}>
-                  Manhã
-                  <div className={styles.line} />
-                </h3>
-                {getClientesMorning().map((cliente: ClienteMetadata) => (
-                  <CardCliente key={cliente.id} cliente={cliente} />
-                ))}
-              </>
-            )}
-
-            {getClientesAfternoon().length > 0 && (
-              <>
-                <h2 className={styles.shift}>
-                  Tarde
-                  <div className={styles.line} />
-                </h2>
-                {getClientesAfternoon().map((cliente: ClienteMetadata) => (
-                  <CardCliente key={cliente.id} cliente={cliente} />
-                ))}
-              </>
-            )}
-
-            {getClientesNight().length > 0 && (
-              <>
-                <h2 className={styles.shift}>
-                  Noite
-                  <div className={styles.line} />
-                </h2>
-                {getClientesNight().map((cliente: ClienteMetadata) => (
-                  <CardCliente key={cliente.id} cliente={cliente} />
-                ))}
-              </>
-            )}
-          </div>
-        </>
-      ) : (
-        <>
-          {clientes.map((cliente: ClienteMetadata) => (
-            <CardCliente key={cliente.id} cliente={cliente} />
-          ))}
-        </>
-      )}
     </>
   );
 }
