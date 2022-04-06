052d2b3451d9c68cf2940bc02be864cbb886ec33
ialexanderbrito <xanderzinho26@gmail.com>
2022-04-06 09:09:26 -0300

    feat: add component dropdown item

---
+++ src/components/DropdownItem/index.tsx
@@ -0,0 +1,29 @@
+import { useEffect } from 'react';
+
+import { DropdownItemProps } from 'types/IComponents';
+
+import { useAuth } from 'hooks/useAuth';
+import { useDropdown } from 'hooks/useDropdown';
+
+import styles from './DropdownItem.module.scss';
+
+export function DropdownItem(props: DropdownItemProps) {
+  const { handleLogout } = useAuth();
+  const { setActiveMenu, setMenuHeight, dropdownRef } = useDropdown();
+
+  useEffect(() => {
+    setMenuHeight(dropdownRef.current?.firstChild.offsetHeight + 30);
+  }, []);
+
+  return (
+    <a
+      href={props.link}
+      className={styles.menuItem}
+      onClick={() => (props.logout ? handleLogout() : props.goToMenu && setActiveMenu(props.goToMenu))}
+    >
+      <span className={styles.iconButton}>{props.leftIcon}</span>
+      {props.children}
+      <span className={styles.iconRight}>{props.rightIcon}</span>
+    </a>
+  );
+}
