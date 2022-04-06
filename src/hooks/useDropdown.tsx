c193d3e8c8631dc811b1c3ec8b19aaa6ef596b6d
ialexanderbrito <xanderzinho26@gmail.com>
2022-04-06 20:12:49 -0300

    fix: get photo dropdown

--- src/hooks/useDropdown.tsx
+++ src/hooks/useDropdown.tsx
@@ -1,8 +1,15 @@
 import { useEffect, useRef, useState } from 'react';
 
+import { getPhoto } from 'services/get/photo';
+
+import { useAuth } from './useAuth';
+
 export function useDropdown() {
+  const { user } = useAuth();
   const [activeMenu, setActiveMenu] = useState('main');
   const [menuHeight, setMenuHeight] = useState<any>(null);
+  const [photo, setPhoto] = useState('');
+  const [name, setName] = useState('');
   const dropdownRef = useRef<any>(null);
 
   function calcHeight(el: { offsetHeight: any }) {
@@ -14,6 +21,26 @@ export function useDropdown() {
     setMenuHeight(dropdownRef.current?.firstChild.offsetHeight + 30);
   }, []);
 
+  async function getPhotoUser(id: string) {
+    const { data, error, status } = await getPhoto(id);
+
+    if (error) {
+      switch (status) {
+        default:
+          throw new Error('Erro ao buscar informações do usuário');
+      }
+    }
+
+    if (!data) return;
+
+    setPhoto(data[0].j[0].src);
+    setName(data[0].j[0].name);
+  }
+
+  useEffect(() => {
+    getPhotoUser(user?.id || '');
+  }, []);
+
   return {
     activeMenu,
     setActiveMenu,
@@ -21,5 +48,7 @@ export function useDropdown() {
     setMenuHeight,
     dropdownRef,
     calcHeight,
+    photo,
+    name,
   };
 }
