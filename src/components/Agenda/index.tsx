e1baf5993331607aa0741f57878f2ceb91643e19
ialexanderbrito <xanderzinho26@gmail.com>
2022-04-07 18:30:24 -0300

    feat: add component agenda modal

---
+++ src/components/Agenda/index.tsx
@@ -0,0 +1,35 @@
+import ICalendarLink from 'react-icalendar-link';
+import { SiApple, SiGooglecalendar } from 'react-icons/si';
+
+import { useAgenda } from 'hooks/useAgenda';
+
+import styles from './Agenda.module.scss';
+
+export function Agenda() {
+  const { handleGoogleCalendarCliente, enventSaveBarbeiro } = useAgenda();
+  return (
+    <>
+      <div className={styles.wrapper}>
+        <h2>Adicionar ao seu calendário</h2>
+
+        <div className={styles.containerButton}>
+          <button
+            className={styles.google}
+            type="button"
+            onClick={() => {
+              handleGoogleCalendarCliente();
+            }}
+          >
+            <SiGooglecalendar />
+            Adicionar ao Google Calendar
+          </button>
+
+          <button className={styles.apple} type="button">
+            <SiApple />
+            <ICalendarLink event={enventSaveBarbeiro}>Adicionar ao Apple Calendar</ICalendarLink>
+          </button>
+        </div>
+      </div>
+    </>
+  );
+}
