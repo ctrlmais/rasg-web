aece97d81427e2bcca9b2fe969709db2d9ae98a0
ialexanderbrito <xanderzinho26@gmail.com>
2022-04-08 17:38:41 -0300

    feat: add button salvar ticket

--- src/pages/MyTicket/index.tsx
+++ src/pages/MyTicket/index.tsx
@@ -1,5 +1,7 @@
 import { SpinnerCircular } from 'spinners-react';
 
+import { Button } from 'components/Button';
+import ComponentToPrint from 'components/ComponentToPrint';
 import { Header } from 'components/Header';
 import { Ticket } from 'components/Ticket';
 
@@ -11,7 +13,7 @@ import styles from './MyTicket.module.scss';
 
 export function MyTicket() {
   const { theme } = useTheme();
-  const { loading, cliente } = useTicket();
+  const { loading, cliente, componentToPrintRef, handleClickPrint } = useTicket();
 
   return (
     <>
@@ -20,7 +22,25 @@ export function MyTicket() {
 
         <div className={styles.container}>
           <h2>Apresente esse ticket para o seu barbeiro</h2>
-          {loading ? <SpinnerCircular color="#ff9000" size={64} /> : <Ticket cliente={cliente} />}
+          {loading ? (
+            <SpinnerCircular color="#ff9000" size={64} />
+          ) : (
+            <>
+              <ComponentToPrint ref={componentToPrintRef}>
+                <Ticket cliente={cliente} />
+              </ComponentToPrint>
+            </>
+          )}
+          <div className={styles.containerButton}>
+            <Button
+              type="button"
+              onClick={() => {
+                handleClickPrint();
+              }}
+            >
+              Salvar ticket
+            </Button>
+          </div>
         </div>
       </div>
     </>
