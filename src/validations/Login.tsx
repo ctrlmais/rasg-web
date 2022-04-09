1d7e20da3ccb3a6f4cff84b14e6a937f12df9d45
ialexanderbrito <xanderzinho26@gmail.com>
2022-04-09 01:24:51 -0300

    fix: validations message

--- src/validations/Login.tsx
+++ src/validations/Login.tsx
@@ -1,6 +1,6 @@
 import * as yup from 'yup';
 
 export const loginSchema = yup.object({
-  email: yup.string().email('Email inválido').required('Campo obrigatório'),
+  email: yup.string().email('Email inválido').required(''),
   senha: yup.string().required('Campo obrigatório'),
 });
