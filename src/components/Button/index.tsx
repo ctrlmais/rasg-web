ff4a9c8b570ba7731f6ce55e3bc3b25dd456209c
ialexanderbrito <xanderzinho26@gmail.com>
2022-04-12 16:11:04 -0300

    feat: styles button disabled

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -3,7 +3,13 @@ import { ButtonProps } from 'types/IComponents';
 import styles from './Button.module.scss';
 export function Button(props: ButtonProps) {
   return (
-    <button type={props.type} className={styles.button} onClick={props.onClick} disabled={props.disabled}>
+    <button
+      type={props.type}
+      className={styles.button}
+      onClick={props.onClick}
+      disabled={props.disabled}
+      style={props.style}
+    >
       {props.children}
     </button>
   );
