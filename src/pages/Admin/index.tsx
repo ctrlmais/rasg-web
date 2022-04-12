6977a596b9f54d3f3f54926cc93d003c6c765f1f
ialexanderbrito <xanderzinho26@gmail.com>
2022-04-12 00:02:21 -0300

    feat: add listagem de barbeiros aprovados

--- src/pages/Admin/index.tsx
+++ src/pages/Admin/index.tsx
@@ -1,4 +1,4 @@
-import { FiCheck } from 'react-icons/fi';
+import { FiCheck, FiSlash } from 'react-icons/fi';
 
 import { SpinnerCircular } from 'spinners-react';
 import { UserMetadata } from 'types/IContext';
@@ -14,7 +14,7 @@ import styles from './Admin.module.scss';
 
 export function Admin() {
   const { theme } = useTheme();
-  const { loading, aproveBarbeiro, barbeiros } = useAdmin();
+  const { loading, aproveBarbeiro, barbeiros, barbeirosAprovados, disabledBarbeiro } = useAdmin();
 
   return (
     <>
@@ -36,16 +36,48 @@ export function Admin() {
               <>
                 {barbeiros.length > 0 &&
                   barbeiros.map((barbeiro: UserMetadata) => (
-                    <div className={styles.containerBarber}>
+                    <div className={styles.containerBarber} key={barbeiro.id}>
                       <CardBarbeiro key={barbeiro.id} barbeiro={barbeiro} />
                       <button
                         className={styles.button}
                         onClick={() => {
-                          console.log('click');
                           aproveBarbeiro(barbeiro?.id);
                         }}
                       >
-                        <FiCheck color="#FFF" size={28} style={{ marginTop: '6px' }} />
+                        <FiCheck color="#FFF" size={18} style={{ marginTop: '6px' }} />
+                      </button>
+                    </div>
+                  ))}
+              </>
+            )}
+          </div>
+          <div className={styles.titleContainer}>
+            {barbeirosAprovados.length > 0 ? (
+              <h2 className={styles.titleHome}>Barbeiros para serem desativados</h2>
+            ) : (
+              <h2 className={styles.titleHome}>Não há barbeiros para serem desativados.</h2>
+            )}
+          </div>
+
+          <div className={styles.containerList}>
+            {loading ? (
+              <SpinnerCircular color="#ff9000" size={64} />
+            ) : (
+              <>
+                {barbeirosAprovados.length > 0 &&
+                  barbeirosAprovados.map((barbeiro: UserMetadata) => (
+                    <div className={styles.containerBarber} key={barbeiro.id}>
+                      <CardBarbeiro key={barbeiro.id} barbeiro={barbeiro} />
+                      <button
+                        style={{
+                          backgroundColor: '#CA0B00',
+                        }}
+                        className={styles.button}
+                        onClick={() => {
+                          disabledBarbeiro(barbeiro?.id);
+                        }}
+                      >
+                        <FiSlash color="#FFF" size={18} style={{ marginTop: '6px' }} />
                       </button>
                     </div>
                   ))}
