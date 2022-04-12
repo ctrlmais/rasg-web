f8f4397c3edd6d6c331bc41616ccbf1d9b66ecae
ialexanderbrito <xanderzinho26@gmail.com>
2022-04-12 16:15:02 -0300

    feat: add services delete agendamento

---
+++ src/services/delete/schedule.tsx
@@ -0,0 +1,7 @@
+import { supabase } from 'services/supabase';
+
+export async function deleteSchedule(idAgendamento: string) {
+  const { data, error } = await supabase.from('tb_schedule').delete().match({ id: idAgendamento });
+
+  return { data, error };
+}
