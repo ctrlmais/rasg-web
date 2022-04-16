20495b6915c26184f6f754cb94c42af969751bda
ialexanderbrito <xanderzinho26@gmail.com>
2022-04-16 19:40:50 -0300

    feat: add click outside menu

--- src/components/NavItem/index.tsx
+++ src/components/NavItem/index.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 
 import { NavItemProps } from 'types/IComponents';
 
@@ -7,17 +7,49 @@ import styles from './NavItem.module.scss';
 export function NavItem(props: NavItemProps) {
   const [open, setOpen] = useState(false);
 
-  return (
-    <li className={styles.navItem}>
-      <a
-        href="#"
-        className={styles.iconButton}
-        onClick={() => (props.children ? setOpen(!open) : props.onClick && props.onClick())}
-      >
-        {props.icon}
-      </a>
+  function useOutsideClick(ref: any) {
+    useEffect(() => {
+      /**
+       * Alert if clicked on outside of element
+       */
+      function handleClickOutside(event: Event) {
+        if (ref.current && !ref.current.contains(event.target)) {
+          setOpen(false);
+        }
+      }
+      // Bind the event listener
+      document.addEventListener('mousedown', handleClickOutside);
+      return () => {
+        // Unbind the event listener on clean up
+        document.removeEventListener('mousedown', handleClickOutside);
+      };
+    }, [ref]);
+  }
 
-      {open && props.children}
-    </li>
+  function OutsideClick({ children }: any) {
+    const wrapperRef = useRef(null);
+    useOutsideClick(wrapperRef);
+
+    return (
+      <div className={styles.navItem} ref={wrapperRef}>
+        {children}
+      </div>
+    );
+  }
+
+  return (
+    <OutsideClick>
+      <li className={styles.navItem}>
+        <a
+          href="#"
+          className={styles.iconButton}
+          onClick={() => (props.children ? setOpen(!open) : props.onClick && props.onClick())}
+        >
+          {props.icon}
+        </a>
+
+        {open && props.children}
+      </li>
+    </OutsideClick>
   );
 }
