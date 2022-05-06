94e53ea01286a479016c9faac95a27820e4812aa
ialexanderbrito <xanderzinho26@gmail.com>
2022-05-06 11:05:52 -0300

    feat: add props ocupacao

--- src/components/Ocupacao/index.tsx
+++ src/components/Ocupacao/index.tsx
@@ -1,29 +1,31 @@
+import { OcupacaoProps } from 'types/IComponents';
+
 import { useAuth } from 'hooks/useAuth';
 
 import styles from './Ocupacao.module.scss';
 
-export function Ocupacao() {
+export function Ocupacao(props: OcupacaoProps) {
   const { ocupacao, setOcupacao } = useAuth();
 
   return (
     <div className={styles.ocupacao}>
       {ocupacao === 'cliente' ? (
         <div className={styles.active} onClick={() => setOcupacao('cliente')}>
-          <p>Sou cliente</p>
+          <p>{props.cliente ? props?.cliente : 'Sou Cliente'}</p>
         </div>
       ) : (
         <div onClick={() => setOcupacao('cliente')} className={styles.none}>
-          <p>Sou cliente</p>
+          <p>{props.cliente ? props?.cliente : 'Sou Cliente'}</p>
         </div>
       )}
 
       {ocupacao === 'barbeiro' ? (
         <div className={styles.active} onClick={() => setOcupacao('barbeiro')}>
-          <p>Sou barbeiro</p>
+          <p>{props.barbeiro ? props?.barbeiro : 'Sou Barbeiro'}</p>
         </div>
       ) : (
         <div onClick={() => setOcupacao('barbeiro')} className={styles.none}>
-          <p>Sou barbeiro</p>
+          <p>{props.barbeiro ? props?.barbeiro : 'Sou Barbeiro'}</p>
         </div>
       )}
     </div>
