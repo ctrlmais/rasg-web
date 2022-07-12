a5566392bafef90882249ac8dd17dcc2e78d1522
ialexanderbrito <xanderzinho26@gmail.com>
2022-07-12 17:33:46 -0300

    feat: add routes history

--- src/routes/PrivateRoutes.tsx
+++ src/routes/PrivateRoutes.tsx
@@ -1,6 +1,7 @@
 import { Routes, Route } from 'react-router-dom';
 
 import { Admin } from 'pages/Admin';
+import { History } from 'pages/History';
 import { Home } from 'pages/Home';
 import { Horarios } from 'pages/Horarios';
 import { MyTicket } from 'pages/MyTicket';
@@ -20,6 +21,7 @@ export function PrivateRoutes() {
       <Route path="/profile" element={<Profile />} />
       <Route path="/p/:id" element={<Schedule />} />
       <Route path="/ticket/:id" element={<MyTicket />} />
+      <Route path="/history" element={<History />} />
       {isBarbeiro && <Route path="/horarios" element={<Horarios />} />}
       {isAlexander && <Route path="/admin" element={<Admin />} />}
       {isBarbeiro && <Route path="/valida/:id" element={<NotFound />} />}
