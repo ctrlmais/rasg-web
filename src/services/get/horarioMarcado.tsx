1ec5f1e3dce25055438dd672f0f8a0e4e19c06b8
ialexanderbrito <xanderzinho26@gmail.com>
2022-07-12 17:26:54 -0300

    chore: add service history monthly

--- src/services/get/horarioMarcado.tsx
+++ src/services/get/horarioMarcado.tsx
@@ -4,6 +4,14 @@ import { supabase } from 'services/supabase';
 
 const atualDayFormatted = format(new Date(), 'yyyy-MM-dd');
 const atualHourFormatted = format(new Date(), 'HH:mm');
+const primeiroDiaMes = format(
+  new Date(new Date().getFullYear(), new Date().getMonth(), 1),
+  'yyyy-MM-dd',
+);
+const ultimoDiaMes = format(
+  new Date(new Date().getFullYear(), new Date().getMonth() + 1, 0),
+  'yyyy-MM-dd',
+);
 
 export async function getHorarioMarcadoCliente(
   clientId: string,
@@ -46,6 +54,25 @@ export async function getHorarioSelecionado(
       p_page: 0,
       p_limit: 15,
       p_orderby: 'hour',
+      p_ascordsc: 'asc',
+    },
+  );
+
+  return { data, error, status };
+}
+
+export async function getHorarioMarcadoMensal(clientId: string, page: number) {
+  const { data, error, status } = await supabase.rpc(
+    'busca_filtrada_schedules',
+    {
+      p_id: [],
+      p_barber_id: [],
+      p_client_id: [clientId],
+      p_dt_inicio: `${primeiroDiaMes}T00:00`,
+      p_dt_fim: `${ultimoDiaMes}T23:59`,
+      p_page: page,
+      p_limit: 5,
+      p_orderby: 'date',
       p_ascordsc: 'asc',
     },
   );
