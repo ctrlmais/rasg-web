c8970e6ba37b5b5fa9b6d712d790cd5eb2010b22
ialexanderbrito <xanderzinho26@gmail.com>
2022-10-06 23:13:56 -0300

    fix: remove services supabase

--- src/services/update/ocupacao.tsx
+++
@@ -1,9 +0,0 @@
-import { supabase } from 'services/supabase';
-
-export async function updateOcupacao(ocupacao: string) {
-  const { error } = await supabase.auth.update({
-    data: { ocupacao },
-  });
-
-  return { error };
-}
