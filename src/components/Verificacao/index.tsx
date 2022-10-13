c958ec0dd50b66cf2e24c8a35185b20331c73a47
ialexanderbrito <xanderzinho26@gmail.com>
2022-10-13 01:21:57 -0300

    fix: component verificacao

--- src/components/Verificacao/index.tsx
+++ src/components/Verificacao/index.tsx
@@ -1,43 +1,30 @@
 import { Button } from 'components/Button';
 
 import { useAuth } from 'hooks/useAuth';
-import { useRegister } from 'hooks/useRegister';
 
 import styles from './Verificacao.module.scss';
 
 export function VerificacaoOcupacao() {
   const { user } = useAuth();
-  const { formikLoginGoogle } = useRegister();
 
   return (
     <>
       <h2 className={styles.titleHome}>
-        Oi {user?.user_metadata.name}, vi que você está logado com o Google.
+        Oi {user?.nmUsuario}, vi que você está logado com o Google.
         <br /> E eu preciso saber se você é:
       </h2>
 
       <form
         onSubmit={(e) => {
           e.preventDefault();
-          formikLoginGoogle.handleSubmit(e);
         }}
       >
         <div className={styles.containerButton}>
-          <Button
-            type="submit"
-            onClick={() => {
-              formikLoginGoogle.setFieldValue('ocupacao', 'barbeiro');
-            }}
-          >
+          <Button type="submit" onClick={() => {}}>
             Barbeiro
           </Button>
 
-          <Button
-            type="submit"
-            onClick={() => {
-              formikLoginGoogle.setFieldValue('ocupacao', 'cliente');
-            }}
-          >
+          <Button type="submit" onClick={() => {}}>
             Cliente
           </Button>
         </div>
