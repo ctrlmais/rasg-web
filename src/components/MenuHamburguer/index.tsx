2bbcd24a391a767732287dea5c3255de4d4e23c7
ialexanderbrito <xanderzinho26@gmail.com>
2022-10-13 01:23:11 -0300

    feat: add menu hamburguer

---
+++ src/components/MenuHamburguer/index.tsx
@@ -0,0 +1,56 @@
+import { FiClock } from 'react-icons/fi';
+import { IoTicketOutline } from 'react-icons/io5';
+import { RiHistoryLine, RiHome2Line } from 'react-icons/ri';
+import { CSSTransition } from 'react-transition-group';
+
+import { DropdownItem } from 'components/DropdownItem';
+
+import { useDropdown } from 'hooks/useDropdown';
+import { usePerfil } from 'hooks/usePerfil';
+
+import styles from './MenuHamburguer.module.scss';
+
+export function MenuHamburguer() {
+  const { isBarbeiro, isCliente } = usePerfil();
+  const { activeMenu, dropdownRef, calcHeight, menuHeight } = useDropdown();
+
+  return (
+    <div
+      className={styles.dropdown}
+      style={{ height: menuHeight }}
+      ref={dropdownRef}
+    >
+      <CSSTransition
+        in={activeMenu === 'main'}
+        timeout={500}
+        classNames="menu-primary"
+        unmountOnExit
+        onEnter={calcHeight}
+      >
+        <div className={styles.menu}>
+          <DropdownItem link="/" leftIcon={<RiHome2Line size={18} />}>
+            Home
+          </DropdownItem>
+
+          <DropdownItem link="/history" leftIcon={<RiHistoryLine size={18} />}>
+            Histórico
+          </DropdownItem>
+
+          {isBarbeiro && (
+            <DropdownItem link="/horarios" leftIcon={<FiClock size={18} />}>
+              Horários
+            </DropdownItem>
+          )}
+          {isCliente && (
+            <DropdownItem
+              link="/ticket"
+              leftIcon={<IoTicketOutline size={18} />}
+            >
+              Ticket
+            </DropdownItem>
+          )}
+        </div>
+      </CSSTransition>
+    </div>
+  );
+}
