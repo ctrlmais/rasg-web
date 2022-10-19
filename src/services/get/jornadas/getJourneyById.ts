a457b36f1b0dd9596eb2459711ee31f2e3b6f50f
ialexanderbrito <xanderzinho26@gmail.com>
2022-10-19 00:12:57 -0300

    fix: remove comment

--- src/services/get/jornadas/getJourneyById.ts
+++ src/services/get/jornadas/getJourneyById.ts
@@ -2,8 +2,6 @@ import { GetJornadaUsuario } from 'types/ServicesProps';
 
 import { api } from 'services/api';
 
-// TODO | ADICIONAR TIPAGEM
-
 export async function getJourneyByIdAWS(id: number) {
   const { data, status } = await api.get<GetJornadaUsuario[]>(
     `jornadas/gerenciador/${id}`,
