acb50006a5bd7abdf230e56d8f53ab24cc21adaa
ialexanderbrito <xanderzinho26@gmail.com>
2022-10-20 15:13:49 -0300

    feat: add update alert pwa

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
+import { useEffect } from 'react';
 import { BrowserRouter } from 'react-router-dom';
+
+import Swal from 'sweetalert2';
 
 import { AuthProvider } from 'contexts/Auth';
 import { ThemeProvider } from 'contexts/Theme';
 import { ToastProvider } from 'contexts/Toast';
 import { UserProvider } from 'contexts/User';
+
+import { useServiceWorker } from 'hooks/useServiceWorker';
 
 import { MainRoutes } from './routes';
 
@@ -11,6 +16,25 @@ import 'react-day-picker/dist/style.css';
 import 'styles/global.scss';
 
 export function App() {
+  const { waitingWorker, showReload, reloadPage } = useServiceWorker();
+
+  useEffect(() => {
+    if (showReload && waitingWorker) {
+      Swal.fire({
+        title: 'Atualização disponível',
+        text: 'Atualize a página para ver as novas atualizações',
+        icon: 'info',
+        confirmButtonColor: '#ff9000',
+        background: '#312e38',
+        color: '#f4ede8',
+        confirmButtonText: 'Atualizar',
+      }).then((result) => {
+        if (result.isConfirmed) {
+          reloadPage();
+        }
+      });
+    }
+  }, [showReload, waitingWorker, reloadPage]);
   return (
     <BrowserRouter>
       <ToastProvider>
