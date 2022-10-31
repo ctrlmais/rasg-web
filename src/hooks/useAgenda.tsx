06b419fbb555fdcfc4f1e6177d512c618dff617a
ialexanderbrito <xanderzinho26@gmail.com>
2022-10-31 20:23:38 -0300

    refactor: hook agenda

--- src/hooks/useAgenda.tsx
+++ src/hooks/useAgenda.tsx
@@ -2,7 +2,6 @@ import { useEffect, useState } from 'react';
 
 import { format } from 'date-fns';
 import { ICalendar } from 'datebook';
-import { ClienteMetadata } from 'types/IContext';
 
 import { useToast } from 'contexts/Toast';
 import { useUser } from 'contexts/User';
@@ -11,47 +10,44 @@ export function useAgenda() {
   const { toast } = useToast();
   const [copied, setCopied] = useState(false);
 
-  const cliente = JSON.parse(
-    localStorage.getItem('cliente') || '',
-  ) as ClienteMetadata;
-  const { generateGoogleCalendarEvent, selectDayFormatted } = useUser();
+  const cliente = JSON.parse(localStorage.getItem('cliente') || '');
+  const { generateGoogleCalendarEvent } = useUser();
 
-  const hourFormattedCalendarCliente = cliente?.hour?.replace(':', '');
-  const hourFormattedCalendarClienteEnd =
-    Number(hourFormattedCalendarCliente) + 100;
-  const hourFormattedCalendarClienteEndFormatted = `${hourFormattedCalendarClienteEnd
-    .toString()
-    .substring(0, 2)}:${hourFormattedCalendarClienteEnd
-    .toString()
-    .substring(2, 4)}`;
-  const dateFormattedCalendar = format(
-    new Date(cliente?.appointment_date),
-    'yyyyMMdd',
+  const dateStartGoogle = format(
+    new Date(cliente.dtInicio),
+    `yyyyMMdd'T'HHmmss`,
   );
-  const startDatesCliente =
-    dateFormattedCalendar + 'T' + hourFormattedCalendarCliente;
-  const endDatesCliente =
-    dateFormattedCalendar + 'T' + hourFormattedCalendarClienteEnd;
+  const dateEndGoogle = format(
+    new Date(cliente.dtTermino),
+    `yyyyMMdd'T'HHmmss`,
+  );
+
+  const dateStartApple = format(
+    new Date(cliente.dtInicio),
+    `yyyy-MM-dd'T'HH:mm:ss`,
+  );
+  const dateEndApple = format(
+    new Date(cliente.dtTermino),
+    `yyyy-MM-dd'T'HH:mm:ss`,
+  );
 
   const eventSaveBarbeiro = new ICalendar({
-    title: `Barba, cabelo e bigode com ${cliente?.client_name}`,
-    description: `Barbearia do ${cliente?.barber_name}`,
-    start: new Date(`${selectDayFormatted}T${cliente?.hour}`),
-    end: new Date(
-      `${selectDayFormatted}T${hourFormattedCalendarClienteEndFormatted}`,
-    ),
+    title: `Barba, cabelo e bigode com ${cliente?.cliente.nmUsuario}`,
+    description: `Barbearia do ${cliente?.gerenciador.nmUsuario}`,
+    start: new Date(dateStartApple),
+    end: new Date(dateEndApple),
     location: '',
   });
 
   function handleGoogleCalendarCliente() {
-    const title = `Barba, cabelo e bigode com ${cliente?.client_name}`;
-    const description = `Barbearia do ${cliente?.barber_name}`;
+    const title = `Barba, cabelo e bigode com ${cliente?.cliente.nmUsuario}`;
+    const description = `Barbearia do ${cliente?.gerenciador.nmUsuario}`;
     const location = '';
 
     generateGoogleCalendarEvent(
       title,
-      startDatesCliente,
-      endDatesCliente,
+      dateStartGoogle,
+      dateEndGoogle,
       description,
       location,
     );
