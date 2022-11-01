445d2ca44a76d36f74784ea48bed8bfcf3f64e3b
ialexanderbrito <xanderzinho26@gmail.com>
2022-11-01 14:23:25 -0300

    chore: hook use barbeiro

--- src/hooks/useBarbeiro.tsx
+++ src/hooks/useBarbeiro.tsx
@@ -3,63 +3,53 @@ import { DateRange } from 'react-day-picker';
 import { useNavigate } from 'react-router-dom';
 
 import { format, addDays } from 'date-fns';
-import Cookies from 'js-cookie';
 import Swal from 'sweetalert2';
-import { ClienteMetadata } from 'types/IContext';
+import { Content } from 'types/ServicesProps';
 import * as XLSX from 'xlsx';
 
 import { useUser } from 'contexts/User';
 
-import { getBarbeiro } from 'services/get/barbeiros';
-import { getClientesMonth } from 'services/get/clientes';
+import { getsShedulesByDateAWS } from 'services/get';
 
 const THIRTYMINUTES = 30 * 60 * 1000;
 const pastMonth = new Date();
 
+interface ExcelDataProps {
+  id: number;
+  Nome: string;
+  Horario: string;
+  Data: string;
+  Turno: string | undefined;
+}
+
 export function useBarbeiro() {
   const navigate = useNavigate();
   const {
-    getFirstCliente,
-    buscaClientesHorario,
+    // getFirstCliente,
+    // buscaClientesHorario,
     buscarClientes,
     clientId,
     getClientesMorning,
     getClientesAfternoon,
     getClientesNight,
     verificaTelefone,
+    situation,
+    verificaHorario,
   } = useUser();
+
   const [toggleDownload, setToggleDownload] = useState(true);
   const [visibleCalendar, setVisibleCalendar] = useState(true);
   const [modalIsOpen, setIsOpen] = useState(false);
   const [date, setDate] = useState(new Date());
-  const [approved, setApproved] = useState('');
   const [ultimaAtualizacao, setUltimaAtualizacao] = useState(
     format(new Date(), 'HH:mm:ss'),
   );
-  const [dataExport, setDataExport] = useState<ClienteMetadata[]>([]);
+  const [dataExport, setDataExport] = useState<ExcelDataProps[]>([]);
   const defaultSelected: DateRange = {
     from: pastMonth,
     to: addDays(pastMonth, 4),
   };
   const [range, setRange] = useState<DateRange | undefined>(defaultSelected);
-  const [loading, setLoading] = useState(true);
-
-  const dataInicial = format(range?.from as Date, 'yyyy-MM-dd');
-  const dataFinal = format(range?.to as Date, 'yyyy-MM-dd');
-
-  function setShiftBarber(turno: string) {
-    if (turno === 'morning') {
-      return 'Manhã';
-    }
-
-    if (turno === 'afternoon') {
-      return 'Tarde';
-    }
-
-    if (turno === 'night') {
-      return 'Noite';
-    }
-  }
 
   function exportToExcel() {
     const ws: XLSX.WorkSheet = XLSX.utils.json_to_sheet(dataExport);
@@ -81,14 +71,14 @@ export function useBarbeiro() {
   }
 
   function isBarbeiroApproved() {
-    if (approved === 'S') {
+    if (situation === 'APROVADO') {
       return true;
-    } else {
-      return false;
     }
+
+    return false;
   }
 
-  function handleSetClienteLocalStorage(cliente: ClienteMetadata) {
+  function handleSetClienteLocalStorage(cliente: Content) {
     localStorage.setItem('cliente', JSON.stringify(cliente));
     openModal();
   }
@@ -110,43 +100,6 @@ export function useBarbeiro() {
   }
 
   useEffect(() => {
-    async function verificarStatusBarbeiro() {
-      setLoading(true);
-      const { data, status, error } = await getBarbeiro(
-        clientId as string,
-        true,
-      );
-
-      if (error) {
-        switch (status) {
-          default:
-            setLoading(false);
-            return;
-        }
-      }
-
-      if (!data) {
-        setLoading(false);
-        return;
-      }
-      if (!data[0].j) {
-        setLoading(false);
-        return;
-      }
-
-      if (data[0].j === null) {
-        setLoading(false);
-        return;
-      }
-
-      setApproved(data[0].j[0].admin_confirmed);
-      setLoading(false);
-    }
-
-    verificarStatusBarbeiro();
-  }, []);
-
-  useEffect(() => {
     const timerID = setInterval(() => tick(), 1000);
 
     return function cleanup() {
@@ -154,19 +107,24 @@ export function useBarbeiro() {
     };
   });
 
-  useEffect(() => {
-    const actualHour = format(date, 'HH:mm:ss');
-    const dateCliente = `${getFirstCliente()?.hour}:00`;
+  // useEffect(() => {
+  //   async function getCliente() {
+  //     const actualHour = format(date, 'HH:mm:ss');
+  //     const dateCliente = `${getFirstCliente()?.dtInicio}`;
+  //     const dateClienteFormat = format(new Date(dateCliente), 'HH:mm:ss');
 
-    const actualHourMinutePlusOne = format(
-      new Date(date.setMinutes(date.getMinutes() + 1)),
-      'HH:mm',
-    );
+  //     const actualHourMinutePlusOne = format(
+  //       new Date(date.setMinutes(date.getMinutes() + 1)),
+  //       'HH:mm',
+  //     );
 
-    if (actualHour === dateCliente) {
-      buscaClientesHorario(actualHourMinutePlusOne);
-    }
-  }, [date]);
+  //     if (actualHour === dateClienteFormat) {
+  //       buscaClientesHorario(actualHourMinutePlusOne);
+  //     }
+  //   }
+
+  //   getCliente();
+  // }, [date]);
 
   useEffect(() => {
     const interval = setInterval(() => {
@@ -181,29 +139,27 @@ export function useBarbeiro() {
 
   useEffect(() => {
     async function buscarDadosParaExcel() {
-      const { data, error, status } = await getClientesMonth(
-        clientId || '',
+      if (!range?.from) return;
+      if (!range?.to) return;
+
+      const dataInicial =
+        format(new Date(range.from), 'yyyy-MM-dd 00:00:00') || '';
+      const dataFinal = format(new Date(range.to), 'yyyy-MM-dd 23:59:59') || '';
+
+      const { data } = await getsShedulesByDateAWS(
         dataInicial,
         dataFinal,
+        Number(clientId),
       );
 
-      if (error) {
-        switch (status) {
-          default:
-            return;
-        }
-      }
-
       if (!data) return;
-      if (!data[0].j) return;
-      if (!data[0].j[0]) return;
 
-      const newValues = data[0].j.map((cliente: ClienteMetadata) => ({
-        id: cliente.id,
-        Nome: cliente.client_name,
-        Horario: cliente.hour,
-        Data: format(new Date(cliente.appointment_date), 'dd/MM/yyyy'),
-        Turno: setShiftBarber(cliente.shift),
+      const newValues = data.content.map((cliente) => ({
+        id: Number(cliente.cliente.cdUsuario),
+        Nome: cliente.cliente.nmUsuario,
+        Horario: format(new Date(cliente.dtInicio), 'HH:mm'),
+        Data: format(new Date(cliente.dtInicio), 'dd/MM/yyyy'),
+        Turno: verificaHorario(format(new Date(cliente.dtInicio), 'HH:mm')),
       }));
 
       setDataExport(newValues);
@@ -212,30 +168,7 @@ export function useBarbeiro() {
     if (toggleDownload === false) {
       buscarDadosParaExcel();
     }
-  }, [dataInicial, dataFinal]);
-
-  useEffect(() => {
-    const barberNewNotification = Cookies.get('barbeiro_notification');
-
-    if (barberNewNotification !== 'success') {
-      Swal.fire({
-        title: 'Novidade no ar!',
-        text: 'Agora você barbeiro pode validar o horário de seus clientes! Basta você ir no menu "Validar Horário" ou abrir sua camera e scanear o QR code do cliente. Essa opção é opcional, mas é util para ter um controle de quem marca e não comparece no horário marcado.',
-        icon: 'info',
-        confirmButtonColor: '#ff9000',
-        cancelButtonColor: '#CA0B00',
-        background: '#312e38',
-        color: '#f4ede8',
-        confirmButtonText: 'Okay',
-      }).then((result) => {
-        if (result.value) {
-          Cookies.set('barbeiro_notification', 'success', {
-            expires: 1,
-          });
-        }
-      });
-    }
-  }, []);
+  }, [toggleDownload, range]);
 
   useEffect(() => {
     if (!verificaTelefone()) {
@@ -264,7 +197,6 @@ export function useBarbeiro() {
       borderRadius: '4px',
       outline: 'none',
       padding: '0px',
-      height: '45vh',
       width: '22rem',
       alignItems: 'center',
       justifyContent: 'center',
@@ -296,7 +228,6 @@ export function useBarbeiro() {
     isBarbeiroApproved: isBarbeiroApproved(),
     visibleCalendar,
     setVisibleCalendar,
-    loading,
     handleSetClienteLocalStorage,
     clienteEqualsZero,
     clienteGreaterZero,
