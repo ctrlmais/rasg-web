ac283a7e537fb7dca618517d91236f0284e28e00
ialexanderbrito <xanderzinho26@gmail.com>
2022-12-17 12:13:40 -0300

    chore: remove services supabase

--- src/services/update/password.tsx
+++
@@ -1,9 +0,0 @@
-import { supabase } from 'services/supabase';
-
-export async function updatePassword(password: string) {
-  const { error } = await supabase.auth.update({
-    password,
-  });
-
-  return { error };
-}
