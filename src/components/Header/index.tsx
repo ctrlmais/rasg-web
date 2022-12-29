0c08fb6cfe8bc2698ded55814d310fa6ef49e468
ialexanderbrito <xanderzinho26@gmail.com>
2022-12-29 09:03:50 -0300

    fix: header button login and register

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,16 +1,24 @@
 import { IoMdMoon, IoMdSunny } from 'react-icons/io';
 import { RiArrowDownSFill } from 'react-icons/ri';
+import { useNavigate } from 'react-router-dom';
 
 import { HeaderProps } from 'types/ComponentsProps';
 
+import { Button } from 'components/Button';
 import { DropdownMenu } from 'components/DropdownMenu';
 import { Navbar } from 'components/Navbar';
 import { NavItem } from 'components/NavItem';
 
 import { useTheme } from 'contexts/Theme';
 
+import { useAuth } from 'hooks/useAuth';
+
+import styles from './Header.module.scss';
+
 export function Header({ logo, back, path }: HeaderProps) {
   const { theme, switchTheme } = useTheme();
+  const { signed } = useAuth();
+  const navigate = useNavigate();
 
   return (
     <Navbar logo={logo} back={back} path={path}>
@@ -21,9 +29,35 @@ export function Header({ logo, back, path }: HeaderProps) {
         icon={theme === 'light' ? <IoMdMoon /> : <IoMdSunny />}
       />
 
-      <NavItem icon={<RiArrowDownSFill />}>
-        <DropdownMenu />
-      </NavItem>
+      {signed ? (
+        <NavItem icon={<RiArrowDownSFill />}>
+          <DropdownMenu />
+        </NavItem>
+      ) : (
+        <div className={styles.buttonLoginContainer}>
+          <h3
+            onClick={() => {
+              navigate('/');
+            }}
+          >
+            Login
+          </h3>
+
+          <Button
+            type="button"
+            style={{
+              height: '1.85rem',
+              width: '8rem',
+              borderRadius: '0.625rem',
+            }}
+            onClick={() => {
+              navigate('/register');
+            }}
+          >
+            Cadastre-se
+          </Button>
+        </div>
+      )}
     </Navbar>
   );
 }
