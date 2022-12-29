d913bea2992bd270dd9e94c0f629f20a666b202c
ialexanderbrito <xanderzinho26@gmail.com>
2022-12-29 09:28:12 -0300

    fix: remove get service types api

--- src/services/get/controllers/getServicesTypes.ts
+++
@@ -1,9 +0,0 @@
-import { GetServicoTipo } from 'types/ServicesProps';
-
-import { api } from 'services/api';
-
-export async function getServicesTypesAWS() {
-  const { data, status } = await api.get<GetServicoTipo[]>(`servicos/tipos`);
-
-  return { data, status };
-}
--- src/services/get/controllers/index.ts
+++ src/services/get/controllers/index.ts
@@ -1,6 +1,5 @@
 export * from './getServicesSituations';
 export * from './getJourneyTypes';
-export * from './getServicesTypes';
 export * from './getsShedulingSituations';
 export * from './getUserSituations';
 export * from './getUserTypes';
