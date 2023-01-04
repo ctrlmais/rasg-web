04e6ed94b6b1efd75a7dabb32e9cbdf6082d91e1
ialexanderbrito <xanderzinho26@gmail.com>
2023-01-04 14:20:28 -0300

    chore: delete default headers

--- src/services/post/auth/postSignIn.ts
+++ src/services/post/auth/postSignIn.ts
@@ -3,6 +3,8 @@ import { PostUsuario } from 'types/ServicesProps';
 import { api } from 'services/api';
 
 export async function postSignInAWS(payload: PostUsuario) {
+  delete api.defaults.headers.common.Authorization;
+
   const { data, status } = await api.post('autenticacoes', payload);
 
   return { data, status };
