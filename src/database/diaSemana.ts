2f1b857a50759a6e7f83a028181f92476566389f
ialexanderbrito <xanderzinho26@gmail.com>
2023-01-09 20:11:42 -0300

    feat: add dia semana database

---
+++ src/database/diaSemana.ts
@@ -0,0 +1,37 @@
+export const diasSemana = [
+  {
+    cdDiaSemana: 0,
+    deDiaSemana: 'D',
+    name: 'Domingo',
+  },
+  {
+    cdDiaSemana: 1,
+    deDiaSemana: 'S',
+    name: 'Segunda-feira',
+  },
+  {
+    cdDiaSemana: 2,
+    deDiaSemana: 'T',
+    name: 'Terça-feira',
+  },
+  {
+    cdDiaSemana: 3,
+    deDiaSemana: 'Q',
+    name: 'Quarta-feira',
+  },
+  {
+    cdDiaSemana: 4,
+    deDiaSemana: 'Q',
+    name: 'Quinta-feira',
+  },
+  {
+    cdDiaSemana: 5,
+    deDiaSemana: 'S',
+    name: 'Sexta-feira',
+  },
+  {
+    cdDiaSemana: 6,
+    deDiaSemana: 'S',
+    name: 'Sábado',
+  },
+];
