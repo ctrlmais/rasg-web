e13bbb14d47c74128ac4def79da12830efb1461a
ialexanderbrito <xanderzinho26@gmail.com>
2023-01-09 20:37:22 -0300

    fix: dropzone maxfile size

--- src/components/Dropzone/index.tsx
+++ src/components/Dropzone/index.tsx
@@ -9,9 +9,11 @@ import { useAuth } from 'hooks/useAuth';
 
 import styles from './Dropzone.module.scss';
 
-const Dropzone: React.FC<DropzoneProps> = ({ onFileUploaded }) => {
-  const { user } = useAuth();
+export function Dropzone({ onFileUploaded }: DropzoneProps) {
+  const { user, profileAvatar } = useAuth();
+
   const [selectedFileUrl, setSelectedFileUrl] = useState('');
+  const maxFileSize = 5242880; // 5MB
 
   const onDrop = useCallback(
     (acceptedFiles: any[]) => {
@@ -24,13 +26,17 @@ const Dropzone: React.FC<DropzoneProps> = ({ onFileUploaded }) => {
     },
     [onFileUploaded],
   );
-  const { getRootProps, getInputProps } = useDropzone({
+  const { getRootProps, getInputProps, fileRejections } = useDropzone({
     onDrop,
     accept: {
       'image/*': [],
     },
     maxFiles: 1,
+    maxSize: maxFileSize,
   });
+
+  const isFileTooLarge =
+    fileRejections.length > 0 && fileRejections[0].file.size > maxFileSize;
 
   return (
     <div className={styles.dropzone} {...getRootProps()}>
@@ -52,15 +58,22 @@ const Dropzone: React.FC<DropzoneProps> = ({ onFileUploaded }) => {
       ) : (
         <div className={styles.profile}>
           <p>
-            <Avvvatars value={user?.nmUsuario || ''} size={160} />
+            {profileAvatar ? (
+              <img
+                src={profileAvatar}
+                alt={user?.nmUsuario}
+                className={styles.preview}
+              />
+            ) : (
+              <Avvvatars value={user?.nmUsuario || ''} size={160} />
+            )}
           </p>
           <div className={styles.upload}>
             <FiCamera />
           </div>
         </div>
       )}
+      {isFileTooLarge && <p>O tamanho máximo é de 5MB.</p>}
     </div>
   );
-};
-
-export default Dropzone;
+}
