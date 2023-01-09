190b0c953e51ba67b2bc003c0a45957e3d57c7e8
ialexanderbrito <xanderzinho26@gmail.com>
2023-01-09 19:34:17 -0300

    feat: add global import assets

---
+++ src/assets/index.ts
@@ -0,0 +1,15 @@
+import imgError404 from './barber-404.svg';
+import imgBarberLogin from './barber.png';
+import imgGoogleLogo from './google-icon.svg';
+import imgRasgDark from './rasg_dark.png';
+import imgRasgLight from './rasg.png';
+import imgBarberRegister from './salao.png';
+
+export {
+  imgError404,
+  imgBarberLogin,
+  imgGoogleLogo,
+  imgRasgDark,
+  imgRasgLight,
+  imgBarberRegister,
+};
