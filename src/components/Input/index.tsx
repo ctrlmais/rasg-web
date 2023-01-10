ce866bed1a976b81067b92f46f3673de5b69c0cf
ialexanderbrito <xanderzinho26@gmail.com>
2023-01-09 23:06:04 -0300

    refactor: component input props and styles

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -2,10 +2,10 @@ import { InputProps } from 'types/ComponentsProps';
 
 import styles from './Input.module.scss';
 
-export function Input(props: InputProps) {
+export function Input({ icon, ...props }: InputProps) {
   return (
     <div className={styles.global}>
-      <span className={styles.icon}>{props.icon}</span>
+      <span className={styles.icon}>{icon}</span>
       <input {...props} className={styles.input} />
     </div>
   );
