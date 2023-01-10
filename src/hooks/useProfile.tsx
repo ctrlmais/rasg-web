8c29edddf28c32eefa37aeb22dc8188cee5049c3
ialexanderbrito <xanderzinho26@gmail.com>
2023-01-10 00:23:17 -0300

    feat: add hook use profile photo and profile update

--- src/hooks/useProfile.tsx
+++ src/hooks/useProfile.tsx
@@ -7,13 +7,17 @@ import { useToast } from 'contexts/Toast';
 
 import { useAuth } from 'hooks/useAuth';
 
-import { putUserAWS } from 'services/update';
+import { getSearchPhotoByHashAWS, getSearchPhotoByIdAWS } from 'services/get';
+import { postFotosAWS } from 'services/post';
+import { putFotosAWS, putUserAWS } from 'services/update';
 
 export function useProfile() {
   const { toast } = useToast();
-  const { user, setUser } = useAuth();
+  const { user, setUser, setProfileAvatar, avatar } = useAuth();
   const [loading, setLoading] = useState(false);
   const [showPassword, setShowPassword] = useState(false);
+
+  const userStorage = JSON.parse(localStorage.getItem('@rasg:user') || '{}');
 
   function showNewPassword() {
     setShowPassword(!showPassword);
@@ -44,6 +48,7 @@ export function useProfile() {
         tipoUsuario: user.tipoUsuario,
         flTermo: 'S',
       };
+
       try {
         setLoading(true);
 
@@ -65,6 +70,15 @@ export function useProfile() {
           formikProfile.setFieldValue('newPassword', '');
           formikProfile.setFieldValue('confirmPassword', '');
         }
+
+        if (!avatar) {
+          const formData = new FormData();
+
+          formData.append('mlArquivo', values.avatar);
+          formData.append('cdUsuario', String(user.cdUsuario));
+
+          await postFotosAWS(formData);
+        }
       } catch (error) {
         toast.error('Não foi possível atualizar perfil', { id: 'toast' });
       } finally {
@@ -73,10 +87,62 @@ export function useProfile() {
     },
   });
 
+  async function putPhotoServices(file: File) {
+    setLoading(true);
+    try {
+      const formData = new FormData();
+
+      const { cdFotoUsuario } = JSON.parse(
+        localStorage.getItem('@rasg:picture') || '{}',
+      );
+
+      formData.append('mlArquivo', file);
+      formData.append('cdUsuario', String(user?.cdUsuario));
+
+      const { status } = await putFotosAWS(formData, cdFotoUsuario);
+
+      if (status === 200) {
+        toast.success('Foto atualizada com sucesso!', { id: 'toast' });
+      }
+    } catch (error) {
+      toast.error('Erro ao atualizar foto!', { id: 'toast' });
+    } finally {
+      setLoading(false);
+    }
+  }
+
   useEffect(() => {
-    formikProfile.setFieldValue('nome', user?.nmUsuario);
-    formikProfile.setFieldValue('email', user?.nmEmail);
-    formikProfile.setFieldValue('phone', user?.nmTelefone);
+    async function getPhoto() {
+      const { data } = await getSearchPhotoByIdAWS(String(user?.cdUsuario));
+
+      localStorage.setItem('@rasg:picture', JSON.stringify(data[0]));
+
+      const { nmHash, nmMime } = data[0];
+
+      const { data: photo } = await getSearchPhotoByHashAWS(nmHash);
+
+      const photoURI = `data:${nmMime};base64,${photo}`;
+
+      setProfileAvatar(photoURI);
+
+      localStorage.setItem('@rasg:avatar', photoURI);
+
+      formikProfile.setFieldValue('avatar', photoURI);
+    }
+
+    getPhoto();
+  }, [user]);
+
+  useEffect(() => {
+    formikProfile.setFieldValue(
+      'nome',
+      user?.nmUsuario || userStorage?.nmUsuario,
+    );
+    formikProfile.setFieldValue('email', user?.nmEmail || userStorage?.nmEmail);
+    formikProfile.setFieldValue(
+      'phone',
+      user?.nmTelefone || userStorage?.nmTelefone,
+    );
   }, [user]);
 
   return {
@@ -84,5 +150,6 @@ export function useProfile() {
     loading,
     showNewPassword,
     showPassword,
+    putPhotoServices,
   };
 }
