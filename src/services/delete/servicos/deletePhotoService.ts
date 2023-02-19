76b0a021535c7a4da2a1ee34f8db161819e29fcc
ialexanderbrito <xanderzinho26@gmail.com>
2023-02-19 14:41:12 -0300

    fix: api implementation

--- src/services/delete/servicos/deletePhotoService.ts
+++ src/services/delete/servicos/deletePhotoService.ts
@@ -1,10 +1,7 @@
 import { api } from 'services/api';
 
 export async function deletePhotoServicesAWS(cdFotoServico: string) {
-  const { data, status } = await api.delete(
-    `servicos/fotos/${cdFotoServico}`,
-    {},
-  );
+  const { data, status } = await api.delete(`servicos/fotos/${cdFotoServico}`);
 
   return { data, status };
 }
--- src/services/delete/servicos/deleteService.ts
+++ src/services/delete/servicos/deleteService.ts
@@ -1,10 +1,7 @@
 import { api } from 'services/api';
 
 export async function deleteServicesAWS(cdServico: string) {
-  const { data, status } = await api.delete(
-    `servicos/${cdServico}/remover`,
-    {},
-  );
+  const { data, status } = await api.delete(`servicos/${cdServico}/remover`);
 
   return { data, status };
 }
